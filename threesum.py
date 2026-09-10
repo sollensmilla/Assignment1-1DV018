40c6ce3b4abfb942865200e435791a7ed0eda79c
Smilla Sollén <ss226uk@student.lnu.se>
2026-09-10 14:30:27 +0200

    Implement full function that searches for threesums with pointer algorithm

--- threesum.py
+++ threesum.py
@@ -22,7 +22,22 @@ def threesum_brute(lst, sum=0):
                     result.add(triplet)
     return list(result)
 
+
 def threesum_pointer(lst, sum=0):
     lst = sorted(lst)
     n = len(lst)
     result = set()
+
+    for i in range(n - 2):
+        left, right = i + 1, n - 1
+        while left < right:
+            current = lst[i] + lst[left] + lst[right]
+            if current == sum:
+                result.add((lst[i], lst[left], lst[right]))
+                left += 1
+                right -= 1
+            elif current < sum:
+                left += 1
+            else:
+                right -= 1
+    return list(result)
