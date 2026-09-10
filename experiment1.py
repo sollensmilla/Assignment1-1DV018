03046869345583501438fbd0fd2d2adbf9951234
Smilla Sollén <ss226uk@student.lnu.se>
2026-09-10 14:34:53 +0200

    Run the new pointer function through the correctness test, results match

--- experiment1.py
+++ experiment1.py
@@ -4,16 +4,19 @@ list of random integers and then run all the algorithms from
 threesum.py.
 """
 
-from threesum import generate_list, threesum_brute
+from threesum import generate_list, threesum_brute, threesum_pointer
 
 
 def test_correctness():
     for run in range(3):
         lst = generate_list(15)
-        result = threesum_brute(lst)
+        result_brute = threesum_brute(lst)
+        result_pointer = threesum_pointer(lst)
+
         print(f"Run {run + 1}")
         print(f"Input: {lst}")
-        print(f"Result: {result}")
+        print(f"Brute result: {result_brute}")
+        print(f"Pointer result: {result_pointer}")
         print()
 
 
