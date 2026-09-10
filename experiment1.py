a1e3c3aa0409c06b8b1e51b777d841d4f7d2ad4a
Smilla Sollén <ss226uk@student.lnu.se>
2026-09-10 13:33:29 +0200

    Implement bruteforce function that returns a list of triple sets

--- experiment1.py
+++ experiment1.py
@@ -7,3 +7,14 @@ def generate_list(n):
 
     return [random.randint(-10 * n, 10 * n) for _ in range(n)]
 
+def threesum_brute(lst, sum=0):
+    n = len(lst)
+    result = set()
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                if lst[i] + lst[j] + lst[k] == sum:
+                    triplet = tuple(sorted((lst[i], lst[j], lst[k])))
+                    result.add(triplet)
+    return list(result)
