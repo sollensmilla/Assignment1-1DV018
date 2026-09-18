bdce84fa648a82821c80cb1383822e6faad55fde
Smilla Sollén <ss226uk@student.lnu.se>
2026-09-18 12:09:42 +0200

    Selection sort and random list generator for part 2

---
+++ sorting.py
@@ -0,0 +1,19 @@
+import random
+
+
+def generate_random_list(n):
+    return [random.randint(-1000, 1000) for _ in range(n)]
+
+
+def selection_sort(lst):
+    result = lst.copy()
+    n = len(result)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if result[j] < result[min_index]:
+                min_index = j
+        result[i], result[min_index] = result[min_index], result[i]
+
+    return result
