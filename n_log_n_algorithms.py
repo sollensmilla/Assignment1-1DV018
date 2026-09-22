222557c7d9705f1624b34f8d04d9a365091d3e48
Smilla Sollén <ss226uk@student.lnu.se>
2026-09-22 11:59:53 +0200

    Merge sort

---
+++ n_log_n_algorithms.py
@@ -0,0 +1,26 @@
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst.copy()
+
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
